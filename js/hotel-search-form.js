435ff26f454a761d77cfde36f3d8666d02e12a5b
Alexey <aleksburlakov@yandex.ru>
2016-09-06 09:41:02 +0300

    Исправил все пункты кроме звезд

--- js/hotel-search-form.js
+++ js/hotel-search-form.js
@@ -3,7 +3,6 @@ var searchForm = document.querySelector(".hotel-search-form");
         
 button.addEventListener("click", function (event) {
     event.preventDefault();
-    console.log("Клик по кнопке"); 
     if (searchForm.classList.contains("hotel-search-form-show")) {
         searchForm.classList.remove("hotel-search-form-show");
     } else {
